036c046da52613c6c03a49437ec357bdcabd8eda
Carter Irish <cdi9919@rit.edu>
2025-09-09 18:02:02 -0400

    Added endpoints and env vars for widget

--- src/index.js
+++ src/index.js
@@ -13,7 +13,12 @@ export default {
 		// This is the entry point for my worker. This is where interaction with API starts
 		// Following two lines are testing if the environment variable is accessible
 		const callback = env.SPOTIFY_CALLBACK_URI;
-		console.log('Callback URL:', callback);
-		return new Response('Callback URL:', callback);
+		const clientID = env.SPOTIFY_CLIENT_ID;
+		// ***secret*** = env.SPOTIFY_CLIENT_SECRET; // secret should not be exposed to client side
+		const authEndpoint = "https://accounts.spotify.com/authorize";
+		const tokenEndpoint = "https://accounts.spotify.com/api/token";
+		const scope = "user-read-currently-playing";
+
+		return new Response("Spotify widget");
 	},
 };
