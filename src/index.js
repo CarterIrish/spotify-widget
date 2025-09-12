81e6b80e709c89db1d568e79a187aca5f420151a
Carter Irish <2023c.irish@gmail.com>
2025-09-12 18:43:06 -0400

    Testing frontend calls

--- src/index.js
+++ src/index.js
@@ -10,43 +10,8 @@
 
 export default {
 	async fetch(request, env, ctx) {
-		// This is the entry point for my worker. This is where interaction with API starts
-		// Following two lines are testing if the environment variable is accessible
-		const callback = env.SPOTIFY_CALLBACK_URI;
-		const clientID = env.SPOTIFY_CLIENT_ID;
-		// ***secret*** = env.SPOTIFY_CLIENT_SECRET; // secret should not be exposed to client side
-		const authEndpoint = "https://accounts.spotify.com/authorize";
-		const tokenEndpoint = "https://accounts.spotify.com/api/token";
-		const baseURL = "https://api.spotify.com/v1";
-		const scope = "user-read-currently-playing";
 
-
-
-		return new Response("Spotify widget");
+		return new Response("Hello Frontend!", { status: 200  });
 	},
 };
-
-function generateRandomString(length) {
-	const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-	const values = crypto.getRandomValues(new Uint8Array(length));
-	return values.reduce((acc, x) => acc + possible[x % possible.length], "");
-}
-
-async function sha256(plain) {
-	const encoder = new TextEncoder()
-	const data = encoder.encode(plain)
-	return window.crypto.subtle.digest('SHA-256', data)
-}
-
-function base64encode(input) {
-	return btoa(String.fromCharCode(...new Uint8Array(input)))
-		.replace(/=/g, '')
-		.replace(/\+/g, '-')
-		.replace(/\//g, '_');
-}
-
-
-
-// const hashed = await sha256(codeVerifier)
-// const codeChallenge = base64encode(hashed);
 
