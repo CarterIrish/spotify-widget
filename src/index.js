521f262481e0ef96517ee01c13d476061d24af29
Carter Irish <2023c.irish@gmail.com>
2025-09-12 18:52:16 -0400

    updates response headers

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@
 export default {
 	async fetch(request, env, ctx) {
 
-		return new Response("Hello Frontend!", { status: 200  });
+		return new Response("Hello Frontend!", {headers: {'Content-type': 'application/json', 'Access-Control-Allow-Origin': '*'}, status: 200  });
 	},
 };
 
